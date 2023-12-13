0603a6294fdaef492aee50e27928463d2282ae43
Kimberlywangari <133366188+Kimberlywangari@users.noreply.github.com>
2023-12-13 17:33:53 +0300

    Add files via upload

---
+++ toDo.js
@@ -0,0 +1,34 @@
+// GROUP WORK
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const submit = document.querySelector('#submit');
+    const newTask = document.querySelector('#task');
+
+    submit.disabled = true;
+
+    newTask.onkeyup = () => {
+        if (newTask.value.length > 0) {
+            submit.disabled = false;
+        }
+        else {
+            submit.disabled = true;
+        }
+    }
+
+    document.querySelector('form').onsubmit = () => {
+
+        const item = newTask.value;
+
+        const list = document.createElement('li');
+        list.innerHTML = item;
+
+        document.querySelector('#tasks').append(list);
+
+        newTask.value = '';
+
+        submit.disabled = true;
+
+        return false;
+    }
+});
